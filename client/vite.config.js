e3a26dae0cbd0987fe19d1196281d92c913637ed
Sebastian Fernandez <sebitafx@gmail.com>
2024-09-08 16:47:55 -0300

    cambio de port

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  host: '0.0.0.0', 
+  port: process.env.PORT || 3004, 
   plugins: [react()],
 })
